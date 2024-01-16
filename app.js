a5cce22681f96c5bcf03cbb02ca616c08bf265fc
kin <kin@topctek.com>
2024-01-16 18:28:59 +0800

    added mongoose successfully

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
 const express = require("express");
-// const mongoose = require("mongoose")
-// require('dotenv').config();  // 要用 .env 存取全域變數就要安裝 dotenv
-// const app = express();
-// const PORT = process.env.PORT;  // 從 .env 取得 PORT 常數
-// const DATABASE_URL = process.env.DATABASE_URL;  // 從 .env 取得 DATABASE_URL 常數
+const mongoose = require("mongoose")
+require('dotenv').config();  // 要用 .env 存取全域變數就要安裝 dotenv
+const app = express();
+const PORT = process.env.PORT;  // 從 .env 取得 PORT 常數
+const DATABASE_URL = process.env.DATABASE_URL;  // 從 .env 取得 DATABASE_URL 常數
 
-// // 連接 mongoDB
-// mongoose
-// .connect(DATABASE_URL)
-// .then(() => console.log("Conneted to MongoDB"))
-// .catch((error) => console.error('Could not connect to MongoDB...', error));
+// 連接 mongoDB
+mongoose
+.connect(DATABASE_URL)
+.then(() => console.log("Conneted to MongoDB"))
+.catch((error) => console.error('Could not connect to MongoDB...', error));
 
-// // 監聽 port:3000  
-// app.listen(PORT, () => {
-//   console.log(`Server is running on PORT : ${PORT}`)
-// })
+// 監聽 port:3000  
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT : ${PORT}`)
+})
 
-// app.get("/RURI", (request, response) => {
-//   response.send("This is RURI.");
-// })
+app.get("/RURI", (request, response) => {
+  response.send("This is RURI.");
+})
 
-// const userSchema = new mongoose.Schema({
-//   name: String,
-// })
-// const User = mongoose.model("User", userSchema, "users");
+const userSchema = new mongoose.Schema({
+  name: String
+})
+const User = mongoose.model("user", userSchema, "user");
 
-// app.get("/queryUser", async (request, response) => {
-//   try {
-//     const users = await User.find();
-//     response.json(users);
-//   } catch (error) {
-//     console.error("ERROR : " + error.message);
-//     response.status(500).send("Error fetching users.");
-//   }
-// });
-
-const { MongoClient } = require('mongodb');
-const url = 'mongodb+srv://hypons:Zz94057704@ruri.wnonyie.mongodb.net/';
-const client = new MongoClient(url);
-
-async function run() {
+app.get("/queryUser", async (request, response) => {
   try {
-    await client.connect();
-    const database = client.db('ruri');
-    const collection = database.collection('user');
-    const users = await collection.find().toArray();
-    console.log(users);
-  } finally {
-    await client.close();
+    const users = await User.find();
+    response.json(users);
+  } catch (error) {
+    console.error("ERROR : " + error.message);
+    response.status(500).send("Error fetching users.");
   }
-}
+});
 
-run();
+// const { MongoClient } = require('mongodb');
+// const url = 'mongodb+srv://hypons:Zz94057704@ruri.wnonyie.mongodb.net/';
+// const client = new MongoClient(url);
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     const database = client.db('ruri');
+//     const collection = database.collection('user');
+//     const users = await collection.find().toArray();
+//     console.log(users);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// run();
